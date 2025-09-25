c41ea78900d62655c5054187f0740aacafdc7254
J-ntk04 <68100306@kmitl.ac.th>
2025-09-25 18:22:43 +0700

    Update todo.js

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,47 +1,48 @@
-window.onload = function() {
-    loadTodos();
-};
+const ftList = document.getElementById("ft_list");
+const newBtn = document.getElementById("newBtn");
 
-function newTodo() {
-    let text = prompt("Enter a new TO DO:");
-    if (text && text.trim() !== "") {
-        addTodo(text.trim());
-        saveTodos();
+function addTask() {
+  const task = prompt("Enter a new task:");
+  if (task !== null && task !== "") {
+    const taskDiv = document.createElement("div");
+    taskDiv.innerHTML = `<li>${task}</li>`;
+    taskDiv.addEventListener("click", removeTask);
+    ftList.insertBefore(taskDiv, ftList.firstChild);
+    saveTasks();
+  }
+}
+
+function removeTask() {
+  const taskDiv = this;
+  const confirmation = confirm("Are you sure you want to remove this task?");
+  if (confirmation) {
+    ftList.removeChild(taskDiv);
+    saveTasks();
+  }
+}
+
+function saveTasks() {
+  const tasks = [];
+  const taskDivs = ftList.children;
+  for (let i = 0; i < taskDivs.length; i++) {
+    tasks.push(taskDivs[i].children[0].innerHTML);
+  }
+  document.cookie = "tasks=" + JSON.stringify(tasks);
+}
+
+function loadTasks() {
+  const tasksStr = document.cookie.replace(/(?:(?:^|.*;\s*)tasks\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+  if (tasksStr) {
+    const tasks = JSON.parse(tasksStr);
+    for (let i = tasks.length-1; i >= 0 ; i--) {
+      const taskDiv = document.createElement("div");
+      taskDiv.innerHTML = `<li>${tasks[i]}</li>`;
+      taskDiv.addEventListener("click", removeTask);
+      ftList.insertBefore(taskDiv, ftList.firstChild);
     }
+  }
 }
 
-function addTodo(text) {
-    let ftList = document.getElementById('ft_list');
-    let newDiv = document.createElement('div');
-    newDiv.className = 'todo-item';
-    newDiv.innerText = text;
+newBtn.addEventListener("click", addTask);
 
-    newDiv.addEventListener('click', function() {
-        if (confirm("Do you want to delete this TO DO?")) {
-            ftList.removeChild(newDiv);
-            saveTodos();
-        }
-    });
-
-    ftList.insertBefore(newDiv, ftList.firstChild);
-}
-
-function saveTodos() {
-    let todos = [];
-    let todoItems = document.querySelectorAll('.todo-item');
-    todoItems.forEach(item => {
-        todos.push(item.innerText);
-    });
-    document.cookie = "todos=" + encodeURIComponent(JSON.stringify(todos)) + "; path=/";
-}
-
-function loadTodos() {
-    let cookies = document.cookie.split(';');
-    let todoData = cookies.find(row => row.trim().startsWith('todos='));
-    if (todoData) {
-        let todos = JSON.parse(decodeURIComponent(todoData.split('=')[1]));
-        todos.forEach(todo => {
-            addTodo(todo);
-        });
-    }
-}
+loadTasks();
